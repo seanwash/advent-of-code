abd7c62b4f29bc0fb9ad84c1280bf3fd1feeceb2
Sean Washington <hello@seanwash.com>
2021-12-02 08:14:13 -0800

    Use Pest's dataset feature to clean up day 1's test

--- tests/Day1/ExerciseTest.php
+++ tests/Day1/ExerciseTest.php
@@ -2,14 +2,10 @@
 
 use SWW\AOC\Day1\Exercise;
 
-it('calculates the number of depth increases', function () {
-    $input = [2, 1, 3];
-    $exercise = new Exercise($input);
-
-    expect($exercise->run())->toBe(1);
-
-    $input = [2, 1, 3, 4, 5];
-    $exercise = new Exercise($input);
-
-    expect($exercise->run())->toBe(3);
-});
+it('calculates the number of depth increases', function ($dataSet, $expectedCount) {
+    $exercise = new Exercise($dataSet);
+    expect($exercise->run())->toBe($expectedCount);
+})->with([
+    [[199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7],
+    [[2, 1, 3, 4, 5], 3]
+]);
